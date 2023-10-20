f428b6702b0e5f9ee59f7d7efcc3d5fd29e64000
felipehsoliveira <felipe.hsoliveira@outlook.com>
2023-10-20 10:24:47 -0300

    objetct value change

--- data/dictionary.ts
+++ data/dictionary.ts
@@ -1,22 +1,22 @@
 const payload = {
   id: 0,
-  username: "xispirito",
-  firstName: "Chapolin",
-  lastName: "Colorado",
-  email: "xispirito_12@email.com",
-  password: "marreta_bionica123",
+  username: "leia_organa12",
+  firstName: "Leia",
+  lastName: "Organa",
+  email: "organa_leia_12@email.com",
+  password: "forTheRepublic123",
   phone: "+5511940028922",
   userStatus: 0
 }
 
 const update_payload = {
   id: 0,
-  username: "xispirito_12",
-  firstName: "xispirot",
-  lastName: "string",
-  email: "string",
-  password: "string",
-  phone: "string",
+  username: "leia_organa13",
+  firstName: "Leia",
+  lastName: "Organa",
+  email: "organa_leia_12@email.com",
+  password: "forTheRepublic123",
+  phone: "+5511940028922",
   userStatus: 0
 }
 
